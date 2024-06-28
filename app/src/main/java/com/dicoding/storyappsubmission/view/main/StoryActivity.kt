8551d249d294ff87c63c9255b0cc05a36a07880c
triyantidwi <triyantidwi23@gmail.com>
2024-06-28 08:02:00 +0700

    commit

--- app/src/main/java/com/dicoding/storyappsubmission/view/main/StoryActivity.kt
+++ app/src/main/java/com/dicoding/storyappsubmission/view/main/StoryActivity.kt
@@ -66,7 +66,7 @@ class StoryActivity : AppCompatActivity() {
             }
 
         )
-        viewModel.story.observe(this) {
+        viewModel.getStoryPagingData()?.observe(this) {
             storyAdapter.submitData(lifecycle, it)
         }
     }
